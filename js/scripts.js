2e93713525277b287e773abd632ef9f96e295977
Skye Atwood and Stephan Khoury <skye@>
2016-10-24 10:49:12 -0700

    add every project file and directory, with HTML and working first step of JS

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("form").submit(function(event) {
+      var nameInput = $("input#favname").val();
+      var colorInput = $("input#favcolor").val();
+      var cityInput = $("input#favcity").val();
+
+      var favthings = [nameInput, colorInput, cityInput];
+
+favthings.pop();
+favthings.reverse();
+favthings.push(cityInput);
+text = "<ul>";
+
+console.log(favthings)
+
+event.preventDefault();
+});
+});
